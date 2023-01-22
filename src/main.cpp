015d0b699dbf20ab2032250cb977a8ef53122187
TheRealRasu <rasu@anton-net.de>
2023-01-22 20:56:30 +0100

    some space allocated

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
-#include <iostream>
-
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
-
+#include <stdlib.h>
+#include <vector>
 
 int main()
 {
+    // registers V0 to VF
+    std::vector<uint8_t> registers (16, 0);
+
+    uint16_t indexRegister {};
+
+    // provide 4 kB of memory (0x000 to 0xFFF)
+    std::vector<uint8_t> memoryData (4096, 0);
     // TODO
 
     return EXIT_SUCCESS;
