0323a9d5a523080686446164f0aec95bbd6c8e49
Rasu <rasu@anton-net.de>
2023-01-23 08:26:17 +0100

    some memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
@@ -6,6 +7,7 @@
 
 int main()
 {
+    std::vector<uint8_t> memory (4096, 0);
     // TODO
 
     return EXIT_SUCCESS;
