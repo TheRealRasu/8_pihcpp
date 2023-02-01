e07aaea379c5a2cc38fbd95548522be20ccd8b2d
TheRealRasu <rasu@anton-net.de>
2023-02-01 10:11:57 +0100

    added debug hw

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+<<<<<<< HEAD
+=======
+
+#include <stdlib.h>
+>>>>>>> added debug hw
 #include <vector>
 
 #define EXIT_SUCCESS 0
@@ -9,6 +14,7 @@ int main()
 {
     std::vector<uint8_t> memory (4096, 0);
     // TODO
+    std::cout << "Hello World!\n";
 
     return EXIT_SUCCESS;
 }
