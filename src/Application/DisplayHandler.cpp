e81cff00550b2dedabfae0f002c9054c5e488b97
TheRealRasu <rasu@anton-net.de>
2024-12-18 21:12:20 +0100

    first image! not fully functioning though

--- src/Application/DisplayHandler.cpp
+++ src/Application/DisplayHandler.cpp
@@ -4,6 +4,7 @@
 
 #include "SDL.h"
 #include "SDL_video.h"
+#include <cstdint>
 
 constexpr int scale = 10;
 
@@ -19,7 +20,7 @@ void DisplayHandler::start()
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    mWindow.reset(SDL_CreateWindow("8_PIHC", gWindowXPos, gWindowYPos, gWindowWidth, gWindowHeight, SDL_WINDOW_SHOWN));
+    mWindow.reset(SDL_CreateWindow("8_PIHC", gWindowXPos, gWindowYPos, gWindowWidth * scale, gWindowHeight * scale, SDL_WINDOW_SHOWN));
     mRenderer.reset(SDL_CreateRenderer(mWindow.get(), -1, SDL_RENDERER_ACCELERATED));
     
     SDL_RenderClear(mRenderer.get());
@@ -38,7 +39,7 @@ bool DisplayHandler::drawSprite(uint16_t xPos, uint16_t yPos, uint8_t spriteHeig
     const uint16_t startX = (xPos % gWindowWidth);
     const uint16_t startY = (yPos % gWindowHeight);
 
-    for (uint8_t yPos = startY; yPos < (startY + spriteHeight); yPos++)
+    for (uint16_t yPos = startY; yPos < (startY + spriteHeight); yPos++)
     {
         if (yPos > gWindowHeight) break;
 
@@ -46,7 +47,7 @@ bool DisplayHandler::drawSprite(uint16_t xPos, uint16_t yPos, uint8_t spriteHeig
 
         uint8_t newColorByte {};
 
-        for (int xPos = startX; xPos < startX + 8; xPos++)
+        for (uint16_t xPos = startX; xPos < startX + 8; xPos++)
         {
             if (xPos > gWindowWidth) break;
 
@@ -86,7 +87,16 @@ void DisplayHandler::drawWindow()
             const uint32_t color = pixel == 0 ? 0x00000000 : 0xFFFFFF00;
             
             SDL_SetRenderDrawColor(mRenderer.get(), (color >> 24) & 0xFF, (color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF);
-            SDL_RenderDrawPoint(mRenderer.get(), xPos, yPos);
+
+            for (uint8_t i = 0; i < scale; i++)
+            {
+                for (uint8_t ii = 0; ii < scale; ii++)
+                {
+                    SDL_RenderDrawPoint(mRenderer.get(), (xPos * scale) + i, (yPos * scale) + ii);
+                }
+            }
         }
     }
+    
+    SDL_RenderPresent(mRenderer.get());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,16 @@ int main(int argc, char** argv)
 {
     if (argc < 2) return EXIT_FAILURE;
 
-
-
     const char* fileName = argv[1];
 
     std::unique_ptr<Application> application = std::make_unique<Application>();
     application->start();
     application->load(fileName);
 
-
     while (gTerminate == false)
     {
         application->update();
-        Sleep(1000 / gFrameRate);
+        Sleep(2);
     }
 
     return EXIT_SUCCESS;
